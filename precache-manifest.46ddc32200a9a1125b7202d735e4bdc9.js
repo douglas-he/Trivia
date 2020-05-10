94b6f4a6079f60405df80fc5c303674b88b33374
douglas-he <doughrqe@gmail.com>
2020-05-09 22:48:33 -0300

    Updates

---
+++ precache-manifest.46ddc32200a9a1125b7202d735e4bdc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60ea5c126d30332db517d978f81346c3",
+    "url": "/Trivia/index.html"
+  },
+  {
+    "revision": "73d5566c835b15e85b65",
+    "url": "/Trivia/static/css/main.113da989.chunk.css"
+  },
+  {
+    "revision": "30756aa4f19d6fba7388",
+    "url": "/Trivia/static/js/2.e97f3487.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/Trivia/static/js/2.e97f3487.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73d5566c835b15e85b65",
+    "url": "/Trivia/static/js/main.aa51a707.chunk.js"
+  },
+  {
+    "revision": "ac27353f8ad5fbc2b84e",
+    "url": "/Trivia/static/js/runtime-main.14d8cce2.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/Trivia/static/media/trivia.466d153e.png"
+  }
+]);
